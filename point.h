2d1b2c6322fa32db8e3669d9cfa07078694e2aee
Andre525 <ghost5run@yandex.ru>
2020-02-22 20:35:32 -0500

    Create point.h

---
+++ point.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+typedef struct point {
+    int x;
+    int y;
+} point_t;
+
+typedef struct circle {
+    point_t A;
+    int R;
+} Circle;
+
+typedef struct triangle {
+    point_t P[4];
+} Triangle;
+
+void Vvod_Koordinat_cirlce(Circle* c);
+void print_cirlce(Circle* c, FILE* f);
+void Vvod_Koordinat_triangle(Triangle* t);
+void print_triangle(Triangle* t, FILE* f);
