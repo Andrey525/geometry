eaee9914dab9c753536420560545a227578bda5b
Andre525 <ghost5run@yandex.ru>
2020-04-29 12:41:32 -0400

    Create intersection.c

---
+++ src/intersection.c
@@ -0,0 +1,11 @@
+#include "point.h"
+#include <math.h>
+
+void top_in_circle(Triangle* t, Circle* c)
+{
+    for (int i = 1; i <= 3; i++) {
+        if ((pow(t->P[i].x - c->A.x, 2) + pow(t->P[i].y - c->A.y, 2)) <= pow(c->R, 2)) {
+            printf("Вершина %d треугольника лежит внутри круга\n", i);
+        }
+    }
+}
